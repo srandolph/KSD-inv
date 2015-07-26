764716ad9fddb28d4d59e5b497503cbdca87f509
Scott Randolph <srandolph@tetrasys.org>
2015-07-25 20:37:12 -0500

    Yet Another Push Up

--- app/app.js
+++ app/app.js
@@ -2,27 +2,27 @@ var app = angular.module('myApp', ['ngRoute']);
 app.factory("services", ['$http', function($http) {
   var serviceBase = 'services/'
     var obj = {};
-    obj.getinventory = function(){
+    obj.getInventory = function(){
         return $http.get(serviceBase + 'inventory');
     }
-    obj.getinventory = function(inventoryID){
+    obj.getInventory = function(inventoryID){
         return $http.get(serviceBase + 'inventory?id=' + inventoryID);
     }
 
-    obj.insertinventory = function (inventory) {
-    return $http.post(serviceBase + 'insertinventory', inventory).then(function (results) {
+    obj.insertInventory = function (inventory) {
+    return $http.post(serviceBase + 'insertInventory', inventory).then(function (results) {
         return results;
     });
 	};
 
-	obj.updateinventory = function (id,inventory) {
-	    return $http.post(serviceBase + 'updateinventory', {id:id, inventory:inventory}).then(function (status) {
+	obj.updateInventory = function (id,inventory) {
+	    return $http.post(serviceBase + 'updateInventory', {id:id, inventory:inventory}).then(function (status) {
 	        return status.data;
 	    });
 	};
 
-	obj.deleteinventory = function (id) {
-	    return $http.delete(serviceBase + 'deleteinventory?id=' + id).then(function (status) {
+	obj.deleteInventory = function (id) {
+	    return $http.delete(serviceBase + 'deleteInventory?id=' + id).then(function (status) {
 	        return status.data;
 	    });
 	};
@@ -38,8 +38,8 @@ app.controller('listCtrl', function ($scope, services) {
 
 app.controller('editCtrl', function ($scope, $rootScope, $location, $routeParams, services, inventory) {
     var inventoryID = ($routeParams.inventoryID) ? parseInt($routeParams.inventoryID) : 0;
-    $rootScope.title = (inventoryID > 0) ? 'Edit inventory' : 'Add inventory';
-    $scope.buttonText = (inventoryID > 0) ? 'Update inventory' : 'Add New inventory';
+    $rootScope.title = (inventoryID > 0) ? 'Edit Inventory' : 'Add Inventory';
+    $scope.buttonText = (inventoryID > 0) ? 'Update Inventory' : 'Add New Inventory';
       var original = inventory.data;
       original._id = inventoryID;
       $scope.inventory = angular.copy(original);
@@ -49,19 +49,19 @@ app.controller('editCtrl', function ($scope, $rootScope, $location, $routeParams
         return angular.equals(original, $scope.inventory);
       }
 
-      $scope.deleteinventory = function(inventory) {
+      $scope.deleteInventory = function(inventory) {
         $location.path('/');
         if(confirm("Are you sure to delete inventory number: "+$scope.inventory._id)==true)
-        services.deleteinventory(inventory.inventoryNumber);
+        services.deleteInventory(inventory.inventoryNumber);
       };
 
-      $scope.saveinventory = function(inventory) {
+      $scope.saveInventory = function(inventory) {
         $location.path('/');
         if (inventoryID <= 0) {
-            services.insertinventory(inventory);
+            services.insertInventory(inventory);
         }
         else {
-            services.updateinventory(inventoryID, inventory);
+            services.updateInventory(inventoryID, inventory);
         }
     };
 });
@@ -70,18 +70,18 @@ app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/', {
-        title: 'inventorys',
-        templateUrl: 'partials/inventorys.html',
+        title: 'Inventory',
+        templateUrl: 'partials/inventory.html',
         controller: 'listCtrl'
       })
       .when('/edit-inventory/:inventoryID', {
-        title: 'Edit inventory',
+        title: 'Edit Inventory',
         templateUrl: 'partials/edit-inventory.html',
         controller: 'editCtrl',
         resolve: {
           inventory: function(services, $route){
             var inventoryID = $route.current.params.inventoryID;
-            return services.getinventory(inventoryID);
+            return services.getInventory(inventoryID);
           }
         }
       })
